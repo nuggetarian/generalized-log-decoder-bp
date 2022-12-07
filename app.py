48eeb4a46256922019f6c7cc9e29e6b9474e6c0b
nuggetarian <michal.zernovic@gmail.com>
2022-12-08 00:29:15 +0100

    Added a print of the original message

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from modules.beautify import Beautify
 from modules.log_array import LogArray
+import json
 
 app = Flask(__name__)
 
@@ -11,8 +12,11 @@ def processmsg():
     # Ziskava raw data
     data = request.data
     s = beautifier.beautify(data)
-    print(s)
+    # print(s)
     arraylog.saveLogs(s)
+    print("===============================================================")
+    resp = json.loads(s)
+    print(resp['event']['original'])
     return s
     
 
