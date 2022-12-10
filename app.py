de5699dbccda536d1af4ba4aec9ffb156bf397d6
nuggetarian <michal.zernovic@gmail.com>
2022-12-10 23:49:24 +0100

    Added primitive filter

--- app.py
+++ app.py
@@ -11,8 +11,23 @@ def processmsg():
     arraylog = LogArray()
     data = request.data # Ziskava raw data
     s = beautifier.beautify(data) # Funkcia na odsadenie JSON
-    print(s) # Print odsadeneho logu
-    arraylog.saveLogs(s)
+    #print(s) # Print odsadeneho logu
+    #arraylog.saveLogs(s)
+    resp = json.loads(s)
+    #Filter na zaklade stanice
+    if resp['host']['hostname'] == "miri":
+        #print(s)
+        print("Miri Log Received.")
+        #arraylog.saveLogs(s)
+    if resp['host']['hostname'] == "radka":
+        #print(s)
+        print("Radka Log Received.")
+        #arraylog.saveLogs(s)
+    if resp['host']['hostname'] == "regina":
+        #print(s)
+        print("Regina Log Received.")
+        #arraylog.saveLogs(s)
+    
     # Kod na pouzitie neskor
     # resp = json.loads(s)
     # print(resp['event']['original']) 
