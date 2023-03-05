6ee24820525ebde8fe087f4e0a8d7df73be930e7
nuggetarian <michal.zernovic@gmail.com>
2023-03-05 01:39:30 +0100

    Fixed minor bugs in windows module

--- modules/systems/windows.py
+++ modules/systems/windows.py
@@ -19,7 +19,7 @@ class Windows:
     # Funkcia na ulozenie pola do suboru
     def dumpWinLogs(self, system, code):
         try:
-            with open(f'exported\\{system}\\winlog_{code}_batch_{BATCH_SIZE}.json', 'w') as f:
+            with open(f'exported\\{system}\\winlog_{code}-batch_{BATCH_SIZE}.json', 'w') as f:
                 json.dump(self.dictionary[f'array_{code}'], f)
         except:
             self.logger.makeLog(4, "log_array", "File Creation Failed")
@@ -37,12 +37,11 @@ class Windows:
             if len(self.dictionary[f'array_{code}']) < BATCH_SIZE and not exists(f'exported\\{system}\\winlog_{code}-batch_{BATCH_SIZE}.json'): 
                 self.dictionary[f'array_{code}'].append(data)
                 print(f"Code of incoming log: {code}, Length of Array: {len(self.dictionary[f'array_{code}'])}")
-                print(self.dictionary.keys())
             # Ak subor uz existuje tak sa nic nerobi
             elif exists(f'exported\\{system}\\winlog_{code}-batch_{BATCH_SIZE}.json'):
                 print("File already exists")
             # Inak sa logy ulozia do suboru a vycisti sa pole (hoci nemusi kedze sa vytvori custom na kazdy kod)
             else: 
                 self.dumpWinLogs(system, code)
-                self.dictionary[f'array_{code}'].clear() #Vycisti sa pole
+                self.dictionary[f'array_{code}'].clear() # Vycisti sa pole
                 self.logger.makeLog(2, "log_array" , f"Windows log file created with a batch size of {BATCH_SIZE}")
