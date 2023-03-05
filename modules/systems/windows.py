45685b83df0e9979c48f84ea419150a7762c1d59
nuggetarian <michal.zernovic@gmail.com>
2023-03-05 01:22:36 +0100

    Fixed Array for windows logs

--- modules/systems/windows.py
+++ modules/systems/windows.py
@@ -8,14 +8,6 @@ class Windows:
     logger = Logger()
     dictionary = {}
     
-    #Funkcia na pridanie windows logu do pola
-    # def arrayWinAppend(self, data):
-    #     self.winlogArray.append(data)
-        
-    # #Getter na windows pole
-    # def getWinArray(self):
-    #     return self.winlogArray
-    
     #Ulozenie pola logov do suboru s cislom globalnej premennej
     def createArray(self, code):
         var_name = 'array_' + code
@@ -30,33 +22,21 @@ class Windows:
                 json.dump(self.dictionary[f'array_{code}'], f)
         except:
             self.logger.makeLog(4, "log_array", "File Creation Failed")
-            # Pridat try catch
         
     def saveWinLog(self, system, data, code):
         if self.getArray(code) is None:
             self.createArray(code)
             self.dictionary[f'array_{code}'].append(data)
             print(f"Code of incoming log: {code}, Length of Array: {len(self.dictionary[f'array_{code}'])}")
-            # print(self.dictionary[f'array_{code}'])
         else:
-            
-        # new_array = [code]
-        # var_name = 'array_' + code
-        # self.new_arrays[var_name] = new_array
-        
-        # print(self.new_arrays[f'array_{code}'])
-            
-            # self.new_arrays[f'array_{code}'] = new_array
-            if len(self.dictionary[f'array_{code}']) < BATCH_SIZE and not exists(f'exported\\{system}\\winlog_{code}-batch_{BATCH_SIZE}.json'): #Porovnanie velkosti pola a batch size, ak je mensie pole tak sa log prida do pola
-                # self.arrayWinAppend(data) #Pridanie logu do pola
+            #Porovnanie velkosti pola a batch size, ak je mensie pole tak sa log prida do pola
+            if len(self.dictionary[f'array_{code}']) < BATCH_SIZE and not exists(f'exported\\{system}\\winlog_{code}-batch_{BATCH_SIZE}.json'): 
                 self.dictionary[f'array_{code}'].append(data)
                 print(f"Code of incoming log: {code}, Length of Array: {len(self.dictionary[f'array_{code}'])}")
                 print(self.dictionary.keys())
-                # print(self.new_arrays[f'array_{code}'])
             elif exists(f'exported\\{system}\\winlog_{code}-batch_{BATCH_SIZE}.json'):
                 print("File already exists")
             else: #Inak sa logy ulozia do suboru a inkrementuje sa cislo ktore pojde do nazvu buduceho suboru
-                # self.arrayWinAppend(data)
                 self.dumpWinLogs(system, code)
                 self.dictionary[f'array_{code}'].clear() #Vycisti sa pole
                 self.logger.makeLog(2, "log_array" , f"Windows log file created with a batch size of {BATCH_SIZE}")
